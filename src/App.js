ecf7cf3dcc3d5cd22f5a2bdbe3ae23c206a06162
Giorgio Tempesta <giorgio.tempesta@gmail.com>
2021-02-23 21:54:09 +0100

    moved all logic in functional component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import './App.css';
 
 // todo find out how to reduce spaces in GitHub visualization
 
-const world = new World(20, 20);
-
 function App() {
   // Declare a new state variable, which we'll call "count"
   const [grid, setGrid] = useState([]);
+  const [world] = useState(() => {
+    return new World(20, 20);
+  });
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     setGrid(world.getFirstGeneration());
     world.printCurrentGeneration();
   }, []);
-  const createWorld = () => {
+  const createNextGeneration = () => {
     const nextGrid = world.getNextGeneration(grid);
-	  // replace the old generation with the new values
-	  setGrid(nextGrid);
-	  world.printCurrentGeneration();
+    // replace the old generation with the new values
+    setGrid(nextGrid);
+    world.printCurrentGeneration();
   } 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={createWorld}>Next generation</button>
+        <button onClick={createNextGeneration}>Next generation</button>
       </header>
     </div>
   );
