109d55efa72a332bac4ae7f2ae7a2ed28a2fb9ab
Giorgio Tempesta <giorgio.tempesta@gmail.com>
2021-02-24 22:51:02 +0100

    Basic timer functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import World from '../classes/World';
 import Grid from './Grid';
 import './App.css';
@@ -10,33 +10,70 @@ function App() {
   // state variables
   const [grid, setGrid] = useState([]);
   const [cellQuantity] = useState(40);
+  
+  // timer variables
+  const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+  const countRef = useRef(null);
+
   const [world] = useState(() => {
     // using a callback to prevent the class from being initialized with every render
     // https://stackoverflow.com/a/64131447/
     return new World(cellQuantity, cellQuantity);
   });
+  
   // only executed when `world` is updated
   useEffect(() => {
     setGrid(world.getFirstGeneration());
     // world.printCurrentGeneration();
   }, [world]);
+  
   const createNextGeneration = () => {
     const nextGrid = world.getNextGeneration(grid);
     // replace the old generation with the new values
     setGrid(nextGrid);
-    // world.printCurrentGeneration();
   }
   const updateCell = (coordinates, value) => {
-    const updatedGrid = world.getUpdatedGeneration(coordinates, value);
-    setGrid(updatedGrid);
-    // world.printCurrentGeneration();
+    if (!isRunning) {
+      // only update if not running
+      const updatedGrid = world.getUpdatedGeneration(coordinates, value);
+      setGrid(updatedGrid);
+    }
   }
+  const handleStart = () => {
+    setIsRunning(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => {
+        return timer + 1;
+      });
+      setGrid((grid) => {
+        const nextGrid = world.getNextGeneration(grid);
+        return nextGrid;
+      });
+    }, 100);
+  }
+
+  const handlePause = () => {
+    clearInterval(countRef.current);
+    setTimer(0);
+    setIsRunning(false);
+  }
+
+  const formatTime = () => {
+    return timer.toString().padStart(2, '0');
+  }
+
   return (
     <div className="App">
       <main className="App-main">
         <h1>Conway's Game of Life</h1>
         <Grid currentGeneration={grid} updateCell={updateCell} />
-        <button onClick={createNextGeneration}>Next generation</button>
+        <p>{formatTime()}</p>
+        <div className="App-controls">
+          <button onClick={handleStart}>Run</button>
+          <button onClick={handlePause}>Stop</button>
+          <button onClick={createNextGeneration} disabled={isRunning}>Next generation</button>
+        </div>
       </main>
     </div>
   );
